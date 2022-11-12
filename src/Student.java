2c23cd4688f56c20df553b817b3fb314d9222c5d
Ayberk <ayberkturksoy97@gmail.com>
2022-11-12 14:20:42 +0300

    Some variable added

--- src/Student.java
+++ src/Student.java
@@ -1,3 +1,7 @@
 public class Student {
    private int studentID;
+   private int semester;
+   private int registerYear;
+   private Lecturer advisor;
+   private ArrayList<DepartmentCourse> allCourses;
 }
