c9711468598a3ba9f097086f77cb77db58dc7839
ezgiern <84743502+ezgiern@users.noreply.github.com>
2022-11-12 16:44:50 +0300

    Create Person.java

--- src/Person.java
+++ src/Person.java
@@ -1,3 +1,26 @@
 public class Person {
-   
-}
+
+        private String name;
+        private Department department;
+
+
+        public String setName() {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public Department setDepartment() {
+            this.department = department;
+        }
+
+        public Department getDepartment() {
+            return department;
+        }
+
+        public String getFaculty() {
+            return department.getFaculty();
+        }
+    }
