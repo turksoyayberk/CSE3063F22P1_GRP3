562c904b1ce17d1e97c761bcf211f3b96340a062
Ayberk <ayberkturksoy97@gmail.com>
2022-11-12 14:29:01 +0300

    Some Setter and Getter methods added

--- src/Student.java
+++ src/Student.java
@@ -4,4 +4,33 @@ public class Student {
    private int registerYear;
    private Lecturer advisor;
    private ArrayList<DepartmentCourse> allCourses;
+
+
+   public int getStudentID() {
+      return studentID;
+   }
+   public void setStudentID(int studentID) {
+      this.studentID = studentID;
+   }
+
+   public int getSemester() {
+      return semester;
+   }
+   public void setSemester(int semester) {
+      this.semester = semester;
+   }
+
+   public int getRegisterYear() {
+        return registerYear;
+   }
+   public void setRegisterYear(int registerYear) {
+      this.registerYear = registerYear;
+   }
+   
+   public Lecturer getAdvisor() {
+      return advisor;
+   }
+   public void setAdvisor(Lecturer advisor) {
+      this.advisor = advisor;
+   }
 }
