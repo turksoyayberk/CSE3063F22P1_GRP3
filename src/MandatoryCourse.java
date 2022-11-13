b57c395d7082c1852de9137269d0102ea372e2e5
beyzalekesiz <109971921+beyzalekesiz@users.noreply.github.com>
2022-11-13 19:57:45 +0300

    MandatoryCourse first implementation

---
+++ src/MandatoryCourse.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+
+public class MandatoryCourse extends DepartmentCourse{
+    private String type;
+    private int capacity;
+
+    public MandatoryCourse(){
+        super();
+        this.type = type;
+    }
+
+    @Override
+    public void addStudent (Student student) {
+        if (hasCapacity()) {
+            getRegisteredStudents().add(student);
+            decreaseCapacity();
+        }
+    };
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public boolean hasCapacity() {
+        return this.capacity != 0;
+    }
+
+    public void increaseCapacity(){
+        this.capacity++;
+    }
+
+    public void decreaseCapacity(){
+        this.capacity--;
+    }
+}
