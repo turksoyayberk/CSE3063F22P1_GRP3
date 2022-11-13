c78bfb2c209e4b05b27c1e6693b32ca87c9c3367
ezgiern <84743502+ezgiern@users.noreply.github.com>
2022-11-13 17:31:15 +0300

    Update Lecturer.java

--- src/Lecturer.java
+++ src/Lecturer.java
@@ -1,3 +1,36 @@
-public class Lecturer {
-   
+import java.util.*;
+
+public class Lecturer extends Person {
+
+    private List<DepartmentCourse> coursesGiven;
+    private List<Student> advisedStudents;
+
+
+    public Lecturer(String name, Department department) {
+
+        super(name,department);
+
+    }
+
+    public List<DepartmentCourse> getcoursesGiven() {
+
+        return coursesGiven;
+    }
+
+    public List<Student> getAdvisedStudents() {
+
+        return advisedStudents;
+    }
+
+    public void addCourse(DepartmentCourse course) {
+
+        coursesGiven.add(course);
+    }
+
+    public boolean removeCourse(DepartmentCourse course) {
+
+        return coursesGiven.remove(course);
+    }
+
+
 }
