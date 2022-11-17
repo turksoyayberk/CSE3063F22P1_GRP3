f5e41a6c5456f61b5bbbf3b452ecc14310c8a67e
Murathan Akman <81395893+Murathanakman@users.noreply.github.com>
2022-11-17 14:48:23 +0300

    Update LabCourse.java
    Revised Constructor & Methods

--- src/LabCourse.java
+++ src/LabCourse.java
@@ -4,30 +4,66 @@ public class LabCourse implements Course{
 	private DepartmentCourse mainCourse;
 	private int capacity;
 	private ArrayList<Student> registeredStudents;
+	private ArrayList<int[]> courseSchedule;
+	private TeachingAssistant lecturer;
+	private int sectionNumber;
+
 	
 	public LabCourse() {
 		this.mainCourse = mainCourse;
-		this.courseId = mainCourse.getCourseId();
+		this.courseId = mainCourse.getCourseId() + "." + sectionNumber;
 		this.registeredStudents = new ArrayList<>();
+		this.sectionNumber = mainCourse.labSectionsCount() + 1;
+		this.courseSchedule = new ArrayList<>();
+		if (this.lecturer != null) {
+            	this.lecturer.assignLabCourse(this);
+       		}
 	}
 	public void addStudent(Student student) {
-		if(hasCapacity()) {
+		if(this.capacity > 0) {
 			registeredStudents.add(student);
 			this.capacity--;
 		}
 	}
+	
+	public void addLectureHour(int day, int lectureHour) {
+        	courseSchedule.add(new int[] { day, lectureHour });
+    	}
+	
 	public void setCapacity(int capacity) {
 		this.capacity = capacity;
 	}
 	public int getCapacity() {
 		return capacity;
 	}
+	public ArrayList<int[]> getCourseSchedule() {
+        	return courseSchedule;
+    	}
 	public boolean hasCapacity() {
-		return true;
+		return this.capacity > 0;
 	}
 	@Override
 	public String getCourseId() {
 		return courseId;
 	}
+	public void decreaseCapacity() {
+        	this.capacity--;
+    	}
+
+    	public DepartmentCourse getMainCourse() {
+        	return mainCourse;
+    	}
+
+    	public TeachingAssistant getLecturer() {
+        	return lecturer;
+    	}
+
+    	public ArrayList<Student> getRegisteredStudents() {
+        	return registeredStudents;
+    	}
+
+    	public int getSectionNumber() {
+        	return sectionNumber;
+    	}
 }
 
