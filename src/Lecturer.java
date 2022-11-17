2915001b2a6a976bc4154f33cbcb34e3356755a6
Murathan Akman <81395893+Murathanakman@users.noreply.github.com>
2022-11-17 14:50:57 +0300

    Update Lecturer.java
    Revised Method Name

--- src/Lecturer.java
+++ src/Lecturer.java
@@ -2,33 +2,27 @@ import java.util.*;
 
 public class Lecturer extends Person {
 
-    private List<DepartmentCourse> coursesGiven;
+    private List<DepartmentCourse> givenCourses;
     private List<Student> advisedStudents;
 
 
     public Lecturer(String name, Department department) {
-
         super(name,department);
-
     }
 
-    public List<DepartmentCourse> getcoursesGiven() {
-
-        return coursesGiven;
+    public List<DepartmentCourse> getGivenCourses() {
+        return givenCourses;
     }
 
     public List<Student> getAdvisedStudents() {
-
         return advisedStudents;
     }
 
     public void addCourse(DepartmentCourse course) {
-
-        coursesGiven.add(course);
+        givenCourses.add(course);
     }
 
     public boolean removeCourse(DepartmentCourse course) {
-
         return coursesGiven.remove(course);
     }
 
