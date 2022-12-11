cb88c4e199fcee1120f09e33bf94e0c9f291b13e
Musa Meriç <musa2001musa@gmail.com>
2022-12-11 13:12:59 +0300

    Update RandomStudentGenerator.java

--- Iter2/src/RandomStudentGenerator.java
+++ Iter2/src/RandomStudentGenerator.java
@@ -8,17 +8,45 @@ import java.util.Map;
 import java.util.logging.Logger;
 
 /**
- * The RandomStudentGenerator class that creates random students with all randomized information and contains related methods
+ * The RandomStudentGenerator class that creates random students with all
+ * randomized information and contains related methods
  */
 public class RandomStudentGenerator {
     /**
      * Student first names String array
      */
-    private final String[] firstNames = {"JALE", "ALI", "MAHMUT", "MANSUR", "GAMZE", "MAHMUT", "YUCEL", "KUBILAY", "HAYATI", "BEDRIYE MUGE", "BIRSEN", "SERDAL", "BUNYAMIN", "OZGUR", "FERDI", "REYHAN", "ILHAN", "GULSAH", "NALAN", "SEMIH", "ERGIN", "FATIH", "SENAY", "SERKAN", "EMRE", "BAHATTIN", "IRAZCA", "HATICE", "BARIS", "REZAN", "FATIH", "FUAT", "GOKHAN", "ORHAN", "MEHMET", "EVREN", "OKTAY", "HARUN", "YAVUZ", "PINAR", "MEHMET", "UMUT", "MESUDE", "HUSEYIN CAHIT", "HASIM ONUR", "EYYUP SABRI", "MUSTAFA", "UFUK", "AHMET ALI", "MEDIHA", "HASAN", "KAMIL", "NEBI", "OZCAN", "NAGIHAN", "CEREN", "SERKAN", "HASAN", "YUSUF KENAN", "TARKAN", "MERAL LEMAN", "ERGIN", "KENAN AHMET", "URAL", "YAHYA", "BENGU", "FATIH NAZMI", "DILEK", "MEHMET", "TUFAN AKIN", "MEHMET", "TURGAY YILMAZ", "GULDEHEN", "GOKMEN", "BULENT", "EROL", "BAHRI", "OZLEM", "SELMA", "TUGSEM", "NAZLI", "MURAT", "EBRU", "AHMET", "HUSEYIN YAVUZ", "BASAK", "AYSEGUL", "EVRIM", "YASER", "ULKU", "OZHAN", "UFUK", "AKSEL", "FULYA", "BURCU", "TAYLAN", "YILMAZ", "ZEYNEP", "BAYRAM", "GULAY", "RABIA", "SEVDA", "SERHAT", "ENGIN", "ASLI", "TUBA", "BARIS", "SEVGI", "KALENDER", "HALIL", "BOLGE", "FERDA", "EZGI", "AYSUN", "SEDA", "OZDEN", "KORAY", "SENEM", "ZEYNEP", "EMEL", "BATURAY KANSU", "NURAY", "AYDOGAN", "DENIZ", "ILKNUR", "TEVFIK", "HASAN SERKAN", "KIRAC", "SEYFI", "SEYMA", "ERSAGUN", "DILBER", "MESUT", "ELIF", "MUHAMMET FATIH", "OZGUR SINAN", "MEHMET OZGUR", "MAHPERI", "ONUR", "FARAH ZEYNEP", "VOLKAN", "HALE", "YENER", "SEDEF", "FADIL", "SULTAN", "MUAMMER HAYRI", };
+    private final String[] firstNames = { "JALE", "ALI", "MAHMUT", "MANSUR", "GAMZE", "MAHMUT", "YUCEL", "KUBILAY",
+            "HAYATI", "BEDRIYE MUGE", "BIRSEN", "SERDAL", "BUNYAMIN", "OZGUR", "FERDI", "REYHAN", "ILHAN", "GULSAH",
+            "NALAN", "SEMIH", "ERGIN", "FATIH", "SENAY", "SERKAN", "EMRE", "BAHATTIN", "IRAZCA", "HATICE", "BARIS",
+            "REZAN", "FATIH", "FUAT", "GOKHAN", "ORHAN", "MEHMET", "EVREN", "OKTAY", "HARUN", "YAVUZ", "PINAR",
+            "MEHMET", "UMUT", "MESUDE", "HUSEYIN CAHIT", "HASIM ONUR", "EYYUP SABRI", "MUSTAFA", "UFUK", "AHMET ALI",
+            "MEDIHA", "HASAN", "KAMIL", "NEBI", "OZCAN", "NAGIHAN", "CEREN", "SERKAN", "HASAN", "YUSUF KENAN", "TARKAN",
+            "MERAL LEMAN", "ERGIN", "KENAN AHMET", "URAL", "YAHYA", "BENGU", "FATIH NAZMI", "DILEK", "MEHMET",
+            "TUFAN AKIN", "MEHMET", "TURGAY YILMAZ", "GULDEHEN", "GOKMEN", "BULENT", "EROL", "BAHRI", "OZLEM", "SELMA",
+            "TUGSEM", "NAZLI", "MURAT", "EBRU", "AHMET", "HUSEYIN YAVUZ", "BASAK", "AYSEGUL", "EVRIM", "YASER", "ULKU",
+            "OZHAN", "UFUK", "AKGUL", "FULYA", "BURCU", "TAYLAN", "YILMAZ", "ZEYNEP", "BAYRAM", "GULAY", "RABIA",
+            "SEVDA", "SERHAT", "ENGIN", "ASLI", "TUBA", "BARIS", "SEVGI", "KALENDER", "HALIL", "BOLGE", "FERDA", "EZGI",
+            "AYSUN", "SEDA", "OZDEN", "KORAY", "SENEM", "ZEYNEP", "EMEL", "BATURAY KANSU", "NURAY", "AYDOGAN", "DENIZ",
+            "ILKNUR", "TEVFIK", "HASAN SERKAN", "KIRAC", "SEYFI", "SEYMA", "ERSAGUN", "DILBER", "MESUT", "ELIF",
+            "MUHAMMET FATIH", "OZGUR SINAN", "MEHMET OZGUR", "MAHPERI", "ONUR", "FARAH ZEYNEP", "VOLKAN", "HALE",
+            "YENER", "SEDEF", "FADIL", "SULTAN", "MUAMMER HAYRI", };
     /*
      * Student last names String array
      */
-    private final String[] lastNames = {"ADA", "KANDEMIR", "ATAK", "ERKURAN", "TASTAN", "OZTURK", "YAZICIOGLU", "VURAL", "YUCEL", "SONMEZ", "ERTEKAN", "DEDE", "UYANIK", "ASLAN", "AKBULUT", "ORHON", "UZ", "YAVUZ", "ERDEM", "KULAC", "KAYA", "SELVA", "AKPINAR", "ABACIOGLU", "ATAY", "ISIK", "ALZER", "OZDEMIR", "OZTURK", "TAHTACI", "BUYUKCAM", "KULAKSIZ", "AKSEL", "EROÄ�LU", "KARAKUM", "DAL", "Ã–CAL", "AYHAN", "YÄ°Ä�Ä°T", "YARBÄ°L", "GÃœMÃœÅ�AY", "MURT", "HALHALLI", "ULUÃ–Z", "Å�EYHANLI", "YILMAZ", "SARAÃ‡OÄ�LU", "SEZER", "DOÄ�AN", "DEMÄ°R", "KAYAYURT", "SÃœRÃœM", "YAVAÅ�Ä°", "TURGUT", "BARBAROS", "ALDÄ°NÃ‡", "TEKÄ°N", "GÃœLÅ�AN", "KÃœFECÄ°LER", "ALMACIOÄ�LU", "Ã‡Ä°LDÄ°R", "TÃœRKDOÄ�AN", "KAYA", "AKNER", "Å�ELÄ°MAN", "YAMAN", "ATÄ°K", "YÄ°Ä�Ä°T", "GÄ°RAY", "YALÃ‡INKAYA", "KILIÃ‡", "Å�ENTÃœRK", "KARABAC", "DEGIRMENCI", "BODUROGLU", "YILDIZ", "GULER", "ERASLAN", "UZER", "PISIRGEN", "ADANIR", "KOC", "KORKMAZ", "YENIDOGAN", "AYDOÄ�AN", "ACARBULUT", "ERGE", "ERDOÄ�AN", "Ã–Ä�ÃœT AYDIN", "KUÅ�KU", "KUCUR TÃœLÃœBAÅ�", "PEKTAÅ�", "KAYACAN", "GÃœLEN", "DOÄ�AN", "CANDAN", "TEMEL", "YENÄ°GÃœN", "YILDIRIM", "BEDER", "AKINCI", "Ã–ZDEMÄ°R", "ONUK", "AYDOÄ�AN", "YILMAZ", "CÃ–MERT", "TOPAL", "KARAHAN", "Å�AHÄ°N", "Ã‡ETÄ°N", "AYTAÃ‡", "KÄ°Å�Ä°", "GÃœNDÃœZ", "AK", "URFALI", "KARAMAN", "MEMETOÄ�LU", "KAZBEK", "KÄ°REÃ‡Ã‡Ä°", "AKIN", "YADÄ°GAROÄ�LU", "YÃœKSEL", "Ã–ZÃ‡ELÄ°K ORAL", "BABUÅ�", "KAPLAN", "AKÃ–Z", "KARTAL", "BÄ°LGÄ°Ã‡", "ERDEN", "TUÄ�CUGÄ°L", "KUMRAL", "ERBAÅ�", "ORAL", "KILAÃ‡", "CENGÄ°Z", "YILDIRIM", "BALABAN"};
+    private final String[] lastNames = { "ADA", "KANDEMIR", "ATAK", "ERKURAN", "TASTAN", "OZTURK", "YAZICIOGLU",
+            "VURAL", "YUCEL", "SONMEZ", "ERTEKAN", "DEDE", "UYANIK", "ASLAN", "AKBULUT", "ORHON", "UZ", "YAVUZ",
+            "ERDEM", "KULAC", "KAYA", "SELVA", "AKPINAR", "ABACIOGLU", "ATAY", "ISIK", "ALZER", "OZDEMIR", "OZTURK",
+            "TAHTACI", "BUYUKCAM", "KULAKSIZ", "AKSEL", "EROÄ�LU", "KARAKUM", "DAL", "Ã–CAL", "AYHAN", "YÄ°Ä�Ä°T",
+            "YARBÄ°L", "GÃœMÃœÅ�AY", "MURT", "HALHALLI", "ULUÃ–Z", "Å�EYHANLI", "YILMAZ", "SARAÃ‡OÄ�LU", "SEZER",
+            "DOÄ�AN", "DEMÄ°R", "KAYAYURT", "SÃœRÃœM", "YAVAÅ�Ä°", "TURGUT", "BARBAROS", "ALDÄ°NÃ‡", "TEKÄ°N",
+            "GÃœLÅ�AN", "KÃœFECÄ°LER", "ALMACIOÄ�LU", "Ã‡Ä°LDÄ°R", "TÃœRKDOÄ�AN", "KAYA", "AKNER", "Å�ELÄ°MAN", "YAMAN",
+            "ATÄ°K", "YÄ°Ä�Ä°T", "GÄ°RAY", "YALÃ‡INKAYA", "KILIÃ‡", "MERIC", "KARABAC", "DEGIRMENCI", "BODUROGLU",
+            "YILDIZ", "GULER", "ERASLAN", "UZER", "PISIRGEN", "ADANIR", "KOC", "KORKMAZ", "YENIDOGAN", "AKSENER",
+            "ACARBULUT", "ERGE", "KILICDAROGLU", "ERDOGAN", "SOYLU", "PEKER", "PEKTAS", "KAYACAN", "GULEN", "DOGAN",
+            "CANDAN", "TEMEL", "YENGAVER", "YILDIRIM", "BEDER", "AKINCI", "OZDEMIR", "ONUK", "AYDOGAN", "YILMAZ",
+            "COMERT", "TOPAL", "KARAHAN", "ATAHAN", "AYTEN", "AYTAS", "KACAR", "GUNDUZ", "AK", "URFALI", "KARAMAN",
+            "MEHMETOGLU", "KAZBEK", "AGAOGLU", "AKIN", "YURDAGUL", "YUKSEL", "MERIC", "BABUS", "KAPLAN", "GANIZ",
+            "KARTAL", "BAS", "ERDEN", "TUGRUL", "KUMRAL", "ERBAS", "ORAL", "KILIC", "CENGIZ", "YILDIRIM", "BALABAN" };
     /**
      * Randomly generated semester year
      */
@@ -88,8 +116,8 @@ public class RandomStudentGenerator {
      */
     public ArrayList<Student> createStudents(Department department, int studentCount) {
         ArrayList<Student> allStudents = new ArrayList<>();
-        int[] fallSemesters = {1, 3, 5, 7};
-        int[] springSemesters = {2, 4, 6, 8};
+        int[] fallSemesters = { 1, 3, 5, 7 };
+        int[] springSemesters = { 2, 4, 6, 8 };
         int[] currentSemesters;
         if (department.getCurrentSemester().equals("fall")) {
             currentSemesters = fallSemesters;
@@ -113,7 +141,7 @@ public class RandomStudentGenerator {
      * @return A random grade
      */
     public static int randomGrade() {
-        int[] baseGrades = {10, 15, 20, 25, 30, 40, 40, 40, 50, 50, 60, 60, 60, 60, 70, 70, 70, 80, 80, 80, 85};
+        int[] baseGrades = { 10, 15, 20, 25, 30, 40, 40, 40, 50, 50, 60, 60, 60, 60, 70, 70, 70, 80, 80, 80, 85 };
         int i = (int) (Math.random() * baseGrades.length);
         return (int) Math.ceil(baseGrades[i] + (Math.random() * 15));
     }
@@ -159,18 +187,19 @@ public class RandomStudentGenerator {
      * @param students List of students
      */
     private void logStudent(List<Student> students) {
-        Logger logger
-                = Logger.getLogger(
+        Logger logger = Logger.getLogger(
                 RandomStudentGenerator.class.getName());
         FileWriter stuWriter;
         try {
             stuWriter = new FileWriter("StuLog.txt");
             for (Student student : students) {
-                StringBuilder studentLog = new StringBuilder("\n" + student.getName() + " (" + student.getStudentID() + ") " + "registered.\n");
+                StringBuilder studentLog = new StringBuilder(
+                        "\n" + student.getName() + " (" + student.getStudentID() + ") " + "registered.\n");
                 for (Map.Entry<DepartmentCourse, StudentCourse> course : student.getNewlyTakenCourses().entrySet()) {
-                    studentLog.append("He/She taken ").append(course.getValue().getMainCourse().getCourseName()).append(" this year \n");
+                    studentLog.append("He/She taken ").append(course.getValue().getMainCourse().getCourseName())
+                            .append(" this year \n");
                 }
-                //logger.log(Level.INFO, String.valueOf(studentLog));
+                // logger.log(Level.INFO, String.valueOf(studentLog));
                 stuWriter.append(studentLog.toString());
                 stuWriter.flush();
             }
